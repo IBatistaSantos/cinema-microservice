086933c9c98a1f83f4f2cdb806e92fa910a9a19e
Israel dos Santos Perreira Batista <ibatista@informs.conder>
2021-06-18 13:45:56 -0300

    Developed update documentation

--- movies-service/src/main/app/index.ts
+++ movies-service/src/main/app/index.ts
@@ -4,10 +4,8 @@ import express from "express";
 import { resolve } from "path";
 import "express-async-errors";
 import swaggerUi from "swagger-ui-express";
-import YAML from "yamljs";
-
 import { connect } from "@/infra/database/mongodb";
-
+import swaggerDocument from "../app/docs/swagger.json";
 import { routes } from "./routes";
 
 connect();
@@ -17,7 +15,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const swaggerDocument = YAML.load(resolve(__dirname, "docs", "swagger.yml"));
+
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use("/api", routes);
